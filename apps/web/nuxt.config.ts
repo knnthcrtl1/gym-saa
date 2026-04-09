92a347b048536e0e08cfe6b43122f521d90a5c71
knnthcrtl1 <knnthcrtl.kc@gmail.com>
2026-04-09 23:15:30 +0800

    fix vue

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -16,8 +16,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase:
-        process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000/api/v1",
+      apiBase: "http://localhost:8000/api/v1",
     },
   },
 });
